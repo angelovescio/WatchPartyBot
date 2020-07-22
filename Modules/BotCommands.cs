1122ecd88883748581e071c598b320764c8922e1
vesh <angelo.vescio@gmail.com>
2020-07-21 23:31:30 -0400

    roles and room join and reset works

--- Modules/BotCommands.cs
+++ Modules/BotCommands.cs
@@ -17,99 +17,8 @@ namespace WatchPartyBot.Modules
     // for commands to be available, and have the Context passed to them, we must inherit ModuleBase
     public class BotCommands : ModuleBase
     {
-        public struct RoleIdName
-        {
-            public ulong Id;
-            public string Role;
-            public RoleIdName(ulong id, string role)
-            {
-                Id = id;
-                Role = role;
-            }
-        }
-        //RoleId List
-        static readonly List<RoleIdName> RoleIDs = new List<RoleIdName>() {
-            new RoleIdName(111,"ExploitDev"),
-            new RoleIdName(222,"CTFTime"),
-            new RoleIdName(333,"Cyber101")
-        };
+        
 
-        // The following example only requires the user to either have the
-        // Administrator permission in this guild or own the bot application.
-        [RequireUserPermission(GuildPermission.ManageRoles, Group = "Permission")]
-        [RequireUserPermission(GuildPermission.ManageChannels, Group = "Permission")]
-        [RequireUserPermission(GuildPermission.KickMembers, Group = "Permission")]
-        [RequireOwner(Group = "Permission")]
-        [Summary("Joins a bot to the voice channel specified and replays desktop audio")]
-        [Command("partytime", RunMode = RunMode.Async)]
-        [Alias("watch", "liveaudio")]
-        public async Task AudioStreamCommand([
-            Summary("Voice Channel name")]
-            IVoiceChannel channel = null,
-            [Summary("Number of audio channels, 1 for mono, 2 for stereo (Default)")]
-            int nAudioChannels = 2,
-            [Summary("Sample rate in hertz, 48000 (Default)")]
-            int sampleRate = 48000,
-            [Summary("Number of bits per sample, 16 (Default)")]
-            int bitsPerSample = 16)
-        {
-            var connection = await channel.ConnectAsync();
-            var dstream = connection.CreatePCMStream(AudioApplication.Mixed);
-
-            using (WasapiCapture soundIn = new WasapiLoopbackCapture())
-            {
-                //initialize the soundIn instance
-                soundIn.Initialize();
-
-                //create a SoundSource around the the soundIn instance
-                //this SoundSource will provide data, captured by the soundIn instance
-                SoundInSource soundInSource = new SoundInSource(soundIn) { FillWithZeros = false };
-
-                //create a source, that converts the data provided by the
-                //soundInSource to any other format
-                //in this case the "Fluent"-extension methods are being used
-                IWaveSource convertedSource = soundInSource
-                    .ChangeSampleRate(sampleRate) // sample rate
-                    .ToSampleSource()
-                    .ToWaveSource(bitsPerSample); //bits per sample
-                //int channels = 2;
-                //channels...
-                using (convertedSource = nAudioChannels == 1 ? convertedSource.ToMono() : convertedSource.ToStereo())
-                {
-                    //register an event handler for the DataAvailable event of 
-                    //the soundInSource
-                    //Important: use the DataAvailable of the SoundInSource
-                    //If you use the DataAvailable event of the ISoundIn itself
-                    //the data recorded by that event might won't be available at the
-                    //soundInSource yet
-                    soundInSource.DataAvailable += (s, e) =>
-                    {
-                        //read data from the converedSource
-                        //important: don't use the e.Data here
-                        //the e.Data contains the raw data provided by the 
-                        //soundInSource which won't have your target format
-                        byte[] buffer = new byte[convertedSource.WaveFormat.BytesPerSecond / 2];
-                        int read;
-
-                        //keep reading as long as we still get some data
-                        //if you're using such a loop, make sure that soundInSource.FillWithZeros is set to false
-                        while ((read = convertedSource.Read(buffer, 0, buffer.Length)) > 0)
-                        {
-                            //write the read data to a file
-                            // ReSharper disable once AccessToDisposedClosure
-                            dstream.Write(buffer, 0, read);
-                        }
-                    };
-
-                    //we've set everything we need -> start capturing data
-                    soundIn.Start();
-
-                    Console.WriteLine("Capturing started ... press any key to stop.");
-                    Console.ReadKey();
-                    soundIn.Stop();
-                }
-            }
-        }
         //scan initial users
         //every hour, grab log entries from starttime to endtime now()
         /*
@@ -119,15 +28,11 @@ namespace WatchPartyBot.Modules
          */
         //
         //allow user to change roles to a new waitlist role, removes all others
-        [RequireUserPermission(GuildPermission.ManageRoles, Group = "Permission")]
-        [RequireUserPermission(GuildPermission.ManageChannels, Group = "Permission")]
-        [RequireUserPermission(GuildPermission.KickMembers, Group = "Permission")]
-        [RequireOwner(Group = "Permission")]
-        [Summary("Opens the room for a specific talk")]
-        [Command("openroom", RunMode = RunMode.Async)]
+        [Summary("Go to the waiting room for a specific talk")]
+        [Command("waitlist", RunMode = RunMode.Async)]
         [Alias("starttalk", "jointalk")]
         public async Task GoToWaitingRoomCommand(
-            [Summary("Room to wait in [ExploitDev, CTFIntro, 101]")]
+            [Summary("Room to wait in [ExploitDev, CTFIntro, Cyber101]")]
             string userRoom = null)
         {
             //get ID for current user
@@ -137,17 +42,20 @@ namespace WatchPartyBot.Modules
             //enumerate all roles current user has
             foreach(ulong id in user.RoleIds)
             {
-                if(RoleIDs.Any(e => (e.Id == id)))
+                if(Program.RoleIDs.Any(e => (e.Id == id)))
                 {
                     IRole rId = Context.Guild.GetRole(id);
                     await user.RemoveRoleAsync(rId);
                 }
             }
             //check if role requested is one of the ones allowed
-            if (RoleIDs.Any(e => e.Role.Contains(userRoom,StringComparison.InvariantCultureIgnoreCase)))
+            if (Program.RoleIDs.Any(e => e.Role.Contains(userRoom,StringComparison.InvariantCultureIgnoreCase)))
             {
-                IRole rId = Context.Guild.GetRole(RoleIDs.Find(f => f.Role.Contains(userRoom, StringComparison.InvariantCultureIgnoreCase)).Id);
-                await user.RemoveRoleAsync(rId);
+                IRole rId = Context.Guild.GetRole(Program.RoleIDs.Where(f => f.Role.Contains(userRoom, StringComparison.InvariantCultureIgnoreCase)).FirstOrDefault().Id);
+                if (rId != null)
+                {
+                    await user.AddRoleAsync(rId);
+                }
             }
         }
         //set up the roles
@@ -160,16 +68,71 @@ namespace WatchPartyBot.Modules
         [Alias("initialize", "setup")]
         public async Task SetupRolesRoomsCommand()
         {
-            //create the channels
-            await Context.Guild.CreateTextChannelAsync("ExploitDev");
-            await Context.Guild.CreateTextChannelAsync("CTFIntro");
-            await Context.Guild.CreateTextChannelAsync("Cyber101");
+            var chans = await Context.Guild.GetChannelsAsync();
+            IGuildChannel cExploitDev = chans.Where(e => e.Name.Contains("ExploitDev",
+               StringComparison.InvariantCultureIgnoreCase)).FirstOrDefault();
+            IGuildChannel cCTFIntro = chans.Where(e => e.Name.Contains("CTFIntro",
+                StringComparison.InvariantCultureIgnoreCase)).FirstOrDefault();
+            IGuildChannel cCyber101 = chans.Where(e => e.Name.Contains("Cyber101",
+                StringComparison.InvariantCultureIgnoreCase)).FirstOrDefault();
+
+            if (cExploitDev == null)
+            {
+                //create the channels
+                ITextChannel tExploitDev = await Context.Guild.CreateTextChannelAsync("ExploitDev");
+                if (tExploitDev != null)
+                {
+                    Program.ChannelIDs.Add(new RoleIdName(tExploitDev.Id, tExploitDev.Name));
+                }
+            }
+            if (cCTFIntro == null)
+            {
+                ITextChannel tCTFIntro = await Context.Guild.CreateTextChannelAsync("CTFIntro");
+                if (tCTFIntro != null)
+                {
+                    Program.ChannelIDs.Add(new RoleIdName(tCTFIntro.Id, tCTFIntro.Name));
+                }
+            }
+            if (cCyber101 == null)
+            {
+                ITextChannel tCyber101 = await Context.Guild.CreateTextChannelAsync("Cyber101");
+                if (tCyber101 != null)
+                {
+                    Program.ChannelIDs.Add(new RoleIdName(tCyber101.Id, tCyber101.Name));
+                }
+            }
 
             //create the roles
-            await Context.Guild.CreateRoleAsync(name: "ExploitDev", color: Color.Teal, isMentionable: false);
-            await Context.Guild.CreateRoleAsync(name: "CTFIntro", color: Color.Purple, isMentionable: false);
-            await Context.Guild.CreateRoleAsync(name: "Cyber101", color: Color.DarkMagenta, isMentionable: false);
-
+            IRole rTryExploitDev = Context.Guild.Roles.Where(e => e.Name.Contains("ExploitDev",
+                StringComparison.InvariantCultureIgnoreCase)).FirstOrDefault();
+            if (rTryExploitDev == null)
+            {
+                IRole rExploitDev = await Context.Guild.CreateRoleAsync(name: "ExploitDev", color: Color.Teal, isMentionable: false);
+                if (rExploitDev != null)
+                {
+                    Program.RoleIDs.Add(new RoleIdName(rExploitDev.Id, rExploitDev.Name));
+                }
+            }
+            IRole rTryCTFIntro = Context.Guild.Roles.Where(e => e.Name.Contains("CTFIntro",
+                StringComparison.InvariantCultureIgnoreCase)).FirstOrDefault();
+            if (rTryCTFIntro == null)
+            {
+                IRole rCTFIntro = await Context.Guild.CreateRoleAsync(name: "CTFIntro", color: Color.Purple, isMentionable: false);
+                if (rCTFIntro != null)
+                {
+                    Program.RoleIDs.Add(new RoleIdName(rCTFIntro.Id, rCTFIntro.Name));
+                }
+            }
+            IRole rTryCyber101 = Context.Guild.Roles.Where(e => e.Name.Contains("Cyber101",
+                StringComparison.InvariantCultureIgnoreCase)).FirstOrDefault();
+            if (rTryCyber101 == null)
+            {
+                IRole rCyber101 = await Context.Guild.CreateRoleAsync(name: "Cyber101", color: Color.DarkMagenta, isMentionable: false);
+                if (rCyber101 != null)
+                {
+                    Program.RoleIDs.Add(new RoleIdName(rCyber101.Id, rCyber101.Name));
+                }
+            }
         }
         //tear it all down
         //delete the roles and rooms
@@ -196,7 +159,7 @@ namespace WatchPartyBot.Modules
                 IRole rCTFIntro = Context.Guild.GetRole(rTryCTFIntro.Id);
                 await rCTFIntro.DeleteAsync();
             }
-            IRole rTryCyber101 = Context.Guild.Roles.Where(e => e.Name.Contains("CTFIntro",
+            IRole rTryCyber101 = Context.Guild.Roles.Where(e => e.Name.Contains("Cyber101",
                 StringComparison.InvariantCultureIgnoreCase)).FirstOrDefault();
             if (rTryCyber101 != null)
             {
@@ -224,6 +187,8 @@ namespace WatchPartyBot.Modules
             {
                 await cCyber101.DeleteAsync();
             }
+            Program.RoleIDs = new HashSet<RoleIdName>();
+            Program.ChannelIDs = new HashSet<RoleIdName>();
         }
 
         //listen for new users entering
@@ -255,7 +220,7 @@ namespace WatchPartyBot.Modules
                 {
                     //get time user came into the server
                 }
-                users.Append(user);
+                users.Add(user);
             }
         }
     }
--- Program.cs
+++ Program.cs
@@ -4,17 +4,39 @@ using Discord.WebSocket;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Threading.Tasks;
 using WatchPartyBot.Services;
 
 namespace WatchPartyBot
 {
+    public class RoleIdName : IEquatable<RoleIdName>
+    {
+        public ulong Id;
+        public string Role;
+        public RoleIdName(ulong id, string role)
+        {
+            Id = id;
+            Role = role;
+        }
+
+        public bool Equals([AllowNull] RoleIdName obj)
+        {
+            return obj is RoleIdName && this.Role.Contains(obj.Role,StringComparison.InvariantCultureIgnoreCase);
+        }
+        
+    }
     class Program
     {
         // setup our fields we assign later
         private readonly IConfiguration _config;
         private DiscordSocketClient _client;
-
+        
+        //RoleId List
+        public static HashSet<RoleIdName> RoleIDs = new HashSet<RoleIdName>();
+        public static HashSet<RoleIdName> ChannelIDs = new HashSet<RoleIdName>();
         static void Main(string[] args)
         {
             new Program().MainAsync().GetAwaiter().GetResult();
@@ -55,17 +77,62 @@ namespace WatchPartyBot
                 await Task.Delay(-1);
             }
         }
+        private void GetChannelsPopulateList()
+        {
+            var guilds = _client.Guilds.Where(e => e.Name.Contains("vesh"));
+            var guild = guilds.FirstOrDefault();
+            var roles = guild.Roles;
+            var chans = guild.Channels;
+            IGuildChannel cExploitDev = chans.Where(e => e.Name.Contains("ExploitDev",
+               StringComparison.InvariantCultureIgnoreCase)).FirstOrDefault();
+            IGuildChannel cCTFIntro = chans.Where(e => e.Name.Contains("CTFIntro",
+                StringComparison.InvariantCultureIgnoreCase)).FirstOrDefault();
+            IGuildChannel cCyber101 = chans.Where(e => e.Name.Contains("Cyber101",
+                StringComparison.InvariantCultureIgnoreCase)).FirstOrDefault();
+            IRole rExploitDev = roles.Where(e => e.Name.Contains("ExploitDev",
+               StringComparison.InvariantCultureIgnoreCase)).FirstOrDefault();
+            IRole rCTFIntro = roles.Where(e => e.Name.Contains("CTFIntro",
+                StringComparison.InvariantCultureIgnoreCase)).FirstOrDefault();
+            IRole rCyber101 = roles.Where(e => e.Name.Contains("Cyber101",
+                StringComparison.InvariantCultureIgnoreCase)).FirstOrDefault();
 
+            if (cExploitDev != null)
+            {
+                Program.ChannelIDs.Add(new RoleIdName(cExploitDev.Id, cExploitDev.Name));
+            }
+            if (cCTFIntro != null)
+            {
+                Program.ChannelIDs.Add(new RoleIdName(cCTFIntro.Id, cCTFIntro.Name));
+            }
+            if (cCyber101 != null)
+            {
+                Program.ChannelIDs.Add(new RoleIdName(cCyber101.Id, cCyber101.Name));
+            }
+
+            //create the roles
+            if (rExploitDev != null)
+            {
+                Program.RoleIDs.Add(new RoleIdName(rExploitDev.Id, rExploitDev.Name));
+            }
+            if (rCTFIntro != null)
+            {
+                Program.RoleIDs.Add(new RoleIdName(rCTFIntro.Id, rCTFIntro.Name));
+            }
+            if (rCyber101 != null)
+            {
+                Program.RoleIDs.Add(new RoleIdName(rCyber101.Id, rCyber101.Name));
+            }
+        }
         private Task LogAsync(LogMessage log)
         {
             Console.WriteLine(log.ToString());
             return Task.CompletedTask;
         }
-
+        
         private Task ReadyAsync()
         {
             Console.WriteLine($"Connected as -> [{_client.CurrentUser}] :)");
-            
+            GetChannelsPopulateList();            
             return Task.CompletedTask;
         }
 
